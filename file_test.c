288d8a6e94404d972df23e56adf5bd320ef0c9a9
duanpeng <duanpeng@p1.com>
2021-01-18 20:33:36 +0800

    up

--- file_test.c
+++ file_test.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#define MAXLEN 1
+void readAndPrintBinaryFile(const char* file);
 int main(int argc, char **argv) {
+	readAndPrintBinaryFile(argv[1]);
+	return 0;
+}
+
+
+void readAndPrintBinaryFile(const char* file) {
+
+	FILE *infile = fopen(file, "rb");
+	if ( infile == NULL ) {
+		printf("Fail to open file!\n");
+		exit(0);  //退出程序（结束程序）
+	}
+	int rc;
+	unsigned char buf[MAXLEN];
+	while ( (rc = fread(buf, sizeof(unsigned char), MAXLEN, infile)) != 0 )
+	{
+		printf("%x ", buf[0]);
+	}
+	fclose(infile);
 }
