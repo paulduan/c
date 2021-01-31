e1896989e3ff8fd50d083349d2933d1d4d8cb05d
duanpeng <duanpeng@p1.com>
2021-01-31 23:46:30 +0800

    up

--- macro_test.c
+++ macro_test.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-void testFunc(void){
-    printf("File = %s\nLine = %d\nFunc=%s\nDate=%s\nTime=%s\n", 
-        __FILE__, __LINE__, __FUNCTION__, __DATE__, __TIME__);
+void testFunc(void) {
+	printf("File = %s\nLine = %d\nFunc=%s\nDate=%s\nTime=%s\n",
+	       __FILE__, __LINE__, __FUNCTION__, __DATE__, __TIME__);
+#if defined(__linux__)
+	printf("linux os");
+#elif defined(__sun)
+	printf("mac os");
+#endif
 }
 
 
 
 int main(int argc, char **argv) {
+	testFunc();
 }
